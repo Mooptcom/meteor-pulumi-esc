89a0c49770a28b54499657ef0afbf3581bad41e4
Kai Chew <kafechew@gmail.com>
2025-04-02 11:54:00 +0800

    fit for Meteor 3 countAsync, insertAsync

--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -1,5 +1,26 @@
 import { Meteor } from 'meteor/meteor';
+import { Posts } from '../imports/api/collections.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // If the Posts collection is empty, add some data
+  if (Posts.find().countAsync() === 0) {
+    const dummyPosts = [
+      {
+        title: 'Introducing Moopt',
+        url: 'https://moopt.com',
+        createdAt: new Date(),
+        author: 'System',
+        votes: 0
+      },
+      {
+        title: 'Discover Moopt',
+        url: 'https://linkedin.com/company/moopt',
+        createdAt: new Date(),
+        author: 'System',
+        votes: 0
+      }
+    ];
+    
+    dummyPosts.forEach(post => Posts.insertAsync(post));
+  }
 });
